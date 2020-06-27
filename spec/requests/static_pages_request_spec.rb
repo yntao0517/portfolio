5bab987f3ab65da395bdc381f37a37a6f8da65b4
Yuta Nakayama <yntao0517@gmail.com>
2020-06-27 16:34:09 +0900

    staticpagesの実装をしました。

--- spec/requests/static_pages_request_spec.rb
+++ spec/requests/static_pages_request_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
+  let(:base_title) {"Coid"}
 
   describe "GET /home" do
+    it "should get root" do
+      get "/"
+      expect(response).to have_http_status(:success)
+      assert_select 'title', "Home | #{base_title}"
+    end
+
     it "returns http success" do
       get "/static_pages/home"
       expect(response).to have_http_status(:success)
+      assert_select 'title', "Home | #{base_title}"
     end
   end
 
