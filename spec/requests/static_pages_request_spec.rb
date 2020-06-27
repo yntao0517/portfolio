30d89df4e4ab58b4a08637e4fb221a3ee00be407
Yuta Nakayama <yntao0517@gmail.com>
2020-06-27 20:08:58 +0900

    titleの設定をしました。

--- spec/requests/static_pages_request_spec.rb
+++ spec/requests/static_pages_request_spec.rb
@@ -7,13 +7,13 @@ RSpec.describe "StaticPages", type: :request do
     it "should get root" do
       get "/"
       expect(response).to have_http_status(:success)
-      assert_select 'title', "Home | #{base_title}"
+      assert_select 'title', "#{base_title}"
     end
 
     it "returns http success" do
       get "/static_pages/home"
       expect(response).to have_http_status(:success)
-      assert_select 'title', "Home | #{base_title}"
+      assert_select 'title', "#{base_title}"
     end
   end
 
