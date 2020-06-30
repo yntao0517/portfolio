9a0fa700cb2a27fa020cdcb0fc8ad8c6ee5b8b2b
Yuta Nakayama <yntao0517@gmail.com>
2020-06-30 23:15:37 +0900

    sessionscontrollerのcreateアクションを実装しました。

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/signup',              to: 'users#new'
   get '/login',               to: 'sessions#new'
-  get '/login',               to: 'sessions#create'
+  post '/login',               to: 'sessions#create'
   delete '/logout',            to: 'sessions#destroy'
   resources :users
 end
