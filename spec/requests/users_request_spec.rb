473fccf200ceede49da5180b382e2a7bfd8d5b71
Yuta Nakayama <yntao0517@gmail.com>
2020-07-02 00:08:43 +0900

    いらないファイルを削除しました。

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  let!(:user) { FactoryBot.create(:user)}
+  let!(:user) { FactoryBot.create(:user) }
 
   describe "GET /new" do
     it "returns http success" do
@@ -16,25 +16,6 @@ RSpec.describe "Users", type: :request do
         sign_in_as user
         get user_path(user)
         expect(response).to be_successful
-      end
-    end
-
-    context "ログインしていないユーザーの場合" do
-      it "ログイン画面にリダイレクトすること" do
-        get user_path(user)
-        expect(response).to redirect_to login_path
-      end
-    end
-
-    context "アカウントが違うユーザーの場合" do
-      before do
-        @other_user = FactoryBot.create(:user)
-      end
-
-      it "ホーム画面にリダイレクトすること" do
-        sign_in_as @other_user
-        get user_path(user)
-        expect(response).to redirect_to root_path
       end
     end
   end
