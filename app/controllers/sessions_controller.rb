b0043c87a5763d9e29d0e61ce81aa28e3bd6fc9e
Yuta Nakayama <yntao0517@gmail.com>
2020-07-02 19:38:02 +0900

    remember_meの実装をしました。

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      remember user
       redirect_back_or user
     else
       flash.now[:danger] = 'メールアドレスまたはパスワードが正しくありません。'
@@ -14,7 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
