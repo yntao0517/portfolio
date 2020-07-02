354c790ee942d1b5bf45d6b099242b0a3a428daa
Yuta Nakayama <yntao0517@gmail.com>
2020-07-02 22:52:17 +0900

    ユーザーの編集、更新のテストを追加しました。

---
+++ spec/features/users_edit_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "UserEdit", type: :feature do
+  let(:user) { FactoryBot.create(:user) }
+
+  scenario "ユーザーは編集に成功する" do
+    valid_login(user)
+    visit user_path(user)
+    click_link "プロフィール変更"
+    fill_in "名前", with: "edit"
+    fill_in "メールアドレス", with: "edit@example.com"
+    fill_in "住所", with: "東京都千代田区霞が関2丁目1-2"
+    fill_in "電話番号", with: "0987654321"
+    click_button "プロフィール更新"
+    expect(current_path).to eq user_path(user)
+    expect(user.reload.email).to eq "edit@example.com"
+  end
+
+  scenario "ユーザーは編集に失敗する" do
+    valid_login(user)
+    visit user_path(user)
+    click_link "プロフィール変更"
+    fill_in "メールアドレス", with: "foo@invalid"
+    fill_in "住所", with: ""
+    fill_in "電話番号", with: ""
+    click_button "プロフィール更新"
+    expect(current_path).to eq user_path(user)
+    expect(user.reload.email).not_to eq "foo@invalid"
+  end
+end
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   let!(:user) { FactoryBot.create(:user) }
+  let!(:other_user) { FactoryBot.create(:user) }
 
   describe "GET /new" do
     it "returns http success" do
@@ -16,6 +17,68 @@ RSpec.describe "Users", type: :request do
         sign_in_as user
         get user_path(user)
         expect(response).to be_successful
+      end
+    end
+  end
+
+  describe "#edit" do
+    context "認可されたユーザーの場合" do
+      it "正常にレスポンスを返すこと" do
+        sign_in_as user
+        get edit_user_path(user)
+        expect(response).to be_successful
+      end
+    end
+
+    context "ログインしていないユーザーの場合" do
+      it "ログイン画面にリダイレクトすること" do
+        get edit_user_path(user)
+        expect(response).to have_http_status "302"
+        expect(response).to redirect_to login_path
+      end
+    end
+
+    context "アカウントが違うユーザーの場合" do
+      it "ホーム画面にリダイレクトすること" do
+        sign_in_as other_user
+        get edit_user_path(user)
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
+
+  describe "#update" do
+    context "認可されたユーザーの場合" do
+      it "ユーザーを更新できること" do
+        user_params = FactoryBot.attributes_for(:user, name: "NewName")
+        sign_in_as user
+        patch user_path(user), params: { id: user.id, user: user_params }
+        expect(user.reload.name).to eq "NewName"
+      end
+    end
+
+    context "ログインしていないユーザーの場合" do
+      it "ログイン画面にリダイレクトすること" do
+        user_params = FactoryBot.attributes_for(:user, name: "NewName")
+        patch user_path(user), params: { id: user.id, user: user_params }
+        expect(response).to have_http_status "302"
+        expect(response).to redirect_to login_path
+      end
+    end
+
+    context "アカウントが違うユーザーの場合" do
+      it "ユーザーを更新できないこと" do
+        user_params = FactoryBot.attributes_for(:user, name: "NewName")
+        sign_in_as other_user
+        patch user_path(user), params: { id: user.id, user: user_params }
+        expect(user.reload.name).to eq other_user.name
+      end
+
+      it "ホーム画面にリダイレクトすること" do
+        user_params = FactoryBot.attributes_for(:user, name: "NewName")
+        sign_in_as other_user
+        patch user_path(user), params: { id: user.id, user: user_params }
+        expect(response).to redirect_to root_path
       end
     end
   end
