723b7b6b672addd577617211693bc02c0a2e1fdd
Yuta Nakayama <yntao0517@gmail.com>
2020-07-03 23:13:42 +0900

    rubocopのエラー解消をしました。

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -10,10 +10,4 @@ class Hospital < ApplicationRecord
   validates :representative, presence: true, length: { maximum: 50 }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
-
-  def Hospital.digest(string)
-    cost ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST:
-                            BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
 end
