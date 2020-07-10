0abb6747089f7c9bfd5ae7a67cae868f9011f759
Yuta Nakayama <yntao0517@gmail.com>
2020-07-10 19:31:00 +0900

    支援希望詳細リストを実装しました。

--- app/controllers/support_lists_controller.rb
+++ app/controllers/support_lists_controller.rb
@@ -3,4 +3,10 @@ class SupportListsController < ApplicationController
     @hospital = Hospital.find(params[:id])
     @hospital_items = @hospital.items.all
   end
+
+  def users
+    @item = Item.find(params[:item_id])
+    item = HospitalItem.find_by(item_id: params[:item_id], hospital_id: params[:hospital_id])
+    @users = item.users.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,5 @@ Rails.application.routes.draw do
     end
   end
   resources :support_lists, only: [:show]
+  post '/support_users',                 to: 'support_lists#users'
 end
