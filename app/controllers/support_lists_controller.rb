4c9acfc6e4a88acf52a446e5606a1cd2f6a6539e
Yuta Nakayama <yntao0517@gmail.com>
2020-07-10 15:27:50 +0900

    支援希望者リストページを作成しました。

--- app/controllers/support_lists_controller.rb
+++ app/controllers/support_lists_controller.rb
@@ -1,2 +1,7 @@
 class SupportListsController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @hospital = Hospital.find(params[:id])
+    @hospital_items = @user.hospital_items.all
+  end
 end
