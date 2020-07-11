5839ceca3e364451ee4101277e3b04fd8cff78f7
Yuta Nakayama <yntao0517@gmail.com>
2020-07-11 20:53:06 +0900

    コントローラの修正を修正をしました。

--- app/controllers/user_chats_controller.rb
+++ app/controllers/user_chats_controller.rb
@@ -1,10 +1,11 @@
 class UserChatsController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    @user = User.find(session[:user])
     @hospital_items = @user.hospital_items
     @hosptial_ids = []
     @hospital_items.each do |item|
       @hospital_ids << item.hospital_id
     end
+    @hospital_ids.uniq!
   end
 end
