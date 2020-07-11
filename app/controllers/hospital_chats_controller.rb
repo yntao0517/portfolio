791812d8dd8b2ab7697a1df51fe1fc01ad194448
Yuta Nakayama <yntao0517@gmail.com>
2020-07-11 20:56:34 +0900

    コントローラの修正を修正をしました。

--- app/controllers/hospital_chats_controller.rb
+++ app/controllers/hospital_chats_controller.rb
@@ -1,6 +1,6 @@
 class HospitalChatsController < ApplicationController
   def show
-  @hospital = Hospital.find(session[:hospital])
+  @hospital = Hospital.find_by(session[:hospital])
   hospital_id = @hospital.id
   hospital_item = HospitalItem.find_by(hospital_id: hospital_id)
     if hospital_item != nil
