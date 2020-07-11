34df6cc9d557addd1ec27824c2b3eef9d60ae243
Yuta Nakayama <yntao0517@gmail.com>
2020-07-11 16:42:51 +0900

    ユーザー側のチャットリストを作成しました。

--- app/controllers/user_chats_controller.rb
+++ app/controllers/user_chats_controller.rb
@@ -1,2 +1,10 @@
 class UserChatsController < ApplicationController
+  def show
+    @hospital = Hospital.find(current_hospital)
+    hospital_id = @hospital.id
+    hospital_item = HospitalItem.find_by(hospital_id: hospital_id)
+    if hospital_item.present?
+      @users = hospital_item.users.all
+    end
+  end
 end
