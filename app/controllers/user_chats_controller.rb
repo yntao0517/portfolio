4160b140d2619881f2823aa6de413a549db9b8b8
Yuta Nakayama <yntao0517@gmail.com>
2020-07-11 20:09:20 +0900

    要素の追加の書き方を変えました。

--- app/controllers/user_chats_controller.rb
+++ app/controllers/user_chats_controller.rb
@@ -4,7 +4,7 @@ class UserChatsController < ApplicationController
     @hospital_items = @user.hospital_items
     @hosptial_ids = []
     @hospital_items.each do |item|
-      @hospital_ids.push(item.hospital_id)
+      @hospital_ids << item.hospital_id
     end
   end
 end
