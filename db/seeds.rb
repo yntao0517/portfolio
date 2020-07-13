2845dd604783b5a1390f4994f10979f5b862e046
Yuta Nakayama <yntao0517@gmail.com>
2020-07-13 11:23:44 +0900

    seedに必要なデータを設定しました。

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,3 +21,24 @@ Hospital.create!(name: "Example Hospital",
               phone_number: phone_number,
               representative: representative )
 end
+
+User.create!(name: "Example User",
+        email: "user@example.com",
+        password: "password",
+        password_confirmation: "password",
+        address: "東京都千代田区霞が関1丁目3-2",
+        phone_number: 1111111111)
+
+99.times do |n|
+  name = Faker::Name.name
+  email = "user-#{n+1}@example.com"
+  password = "password"
+  address = "東京都千代田区霞が関1丁目3-2"
+  phone_number = 1111111111
+  User.create!(name: name,
+              email: email,
+              password: password,
+              password_confirmation: password,
+              address: address,
+              phone_number: phone_number)
+end
